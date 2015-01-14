5fe47bad8e7fb248d34c8bbfbd83465843e2768e
jangelroa <jangelroa@gmail.com>
2015-01-14 00:40:34 -0800

    Changed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   post 'users/attending_event'
   post 'users/rsvp'
 
-  root 'users/index'
+  root 'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
